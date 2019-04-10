ac00069919d178dedffcc722bbc025802c512ce6
Artem Uznazov <artem.uznazov@hotmail.com>
2019-04-10 22:12:27 +0300

    Add project files.

---
+++ OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,14 @@
+﻿using OdeToFood.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurants> GetAll();
+    }
+
+
+}
